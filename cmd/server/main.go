5adc40360252132217cd6d4dacc4eec23e104f7b
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-07-01 23:03:58 -0300

    update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	if err := router.Run(); err != nil {
+		log.Fatal("failed to start the server. err:", err)
+	}
+
+}
