9349c850abd829e812452fbf012ce2ed5b2232f6
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-07-01 21:30:53 -0300

    update file slq_queries.go

--- internal/employee/repository/mariadb/sql_queries.go
+++ internal/employee/repository/mariadb/sql_queries.go
@@ -21,4 +21,11 @@ const (
 	queryCreate = `INSERT INTO employees 
 	(card_number_id, first_name, last_name, warehouse_id) 
 	VALUES (?, ?, ?, ?)`
+
+	queryUpdate = `UPDATE employees SET
+	card_number_id=?, 
+	first_name=?, 
+	last_name=?, 
+	warehouse_id=?
+	WHERE ID =?`
 )
