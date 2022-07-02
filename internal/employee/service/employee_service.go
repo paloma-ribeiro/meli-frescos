03986b998acf36632a3e928256ee04b1e62e23ac
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-07-01 22:42:06 -0300

    created function GetAll in file employee_service.go

--- internal/employee/service/employee_service.go
+++ internal/employee/service/employee_service.go
@@ -1,1 +1,25 @@
 package service
+
+import (
+	"context"
+
+	"github.com/paloma-ribeiro/meli-frescos/internal/employee/domain"
+)
+
+type employeeService struct {
+	repository domain.EmployeeRepository
+}
+
+func NewEmployee(er domain.EmployeeRepository) domain.EmployeeService {
+	return &employeeService{repository: er}
+}
+
+func (e employeeService) GetAll(ctx context.Context) (*[]domain.Employee, error) {
+	employees, err := e.repository.GetAll(ctx)
+
+	if err != nil {
+		return employees, err
+	}
+
+	return employees, nil
+}
