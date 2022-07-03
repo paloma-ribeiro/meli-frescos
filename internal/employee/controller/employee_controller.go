22d4478ecee0cdb01a1ddb2745ee59b4719d2874
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-07-03 13:19:25 -0300

    created constructor to EmployeeController in file employee_controller.go

--- internal/employee/controller/employee_controller.go
+++ internal/employee/controller/employee_controller.go
@@ -1,1 +1,19 @@
 package controller
+
+import (
+	"github.com/paloma-ribeiro/meli-frescos/internal/employee/domain"
+)
+
+type EmployeeController struct {
+	service domain.EmployeeService
+}
+
+func NewEmployeeController(service domain.EmployeeService) (*EmployeeController, error) {
+	if service == nil {
+		return nil, domain.ErrInvalidService
+	}
+
+	return &EmployeeController{
+		service: service,
+	}, nil
+}
