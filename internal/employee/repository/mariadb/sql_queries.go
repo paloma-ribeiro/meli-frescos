968264cdb2b9c22e84046bff1d35bf3db8783b3c
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-07-05 11:47:04 -0300

    uploaded sintax queries in file sql_queries.go

--- internal/employee/repository/mariadb/sql_queries.go
+++ internal/employee/repository/mariadb/sql_queries.go
@@ -2,24 +2,23 @@ package mariadb
 
 const (
 	queryGetAll = `SELECT 
-	id
-	card_number_id
-	first_name
-	last_name
+	id,
+	card_number_id,
+	first_name,
+	last_name,
 	warehouse_id
 	FROM employees`
 
 	queryGetById = `SELECT 
-	id
-	card_number_id
-	first_name
-	last_name
+	id,
+	card_number_id,
+	first_name,
+	last_name,
 	warehouse_id
 	FROM employees
 	WHERE ID = ?`
 
-	queryCreate = `INSERT INTO employees 
-	(card_number_id, first_name, last_name, warehouse_id) 
+	queryCreate = `INSERT INTO employees (card_number_id, first_name, last_name, warehouse_id) 
 	VALUES (?, ?, ?, ?)`
 
 	queryUpdate = `UPDATE employees SET
